62d7de3fd81693c69cfef1d1e0cb47a54e6572eb
Andrea Proia <a.proia@cineca.it>
2025-01-07 14:11:55 +0100

    fix arg in build_index

--- vectoria_lib/tasks/build_index.py
+++ vectoria_lib/tasks/build_index.py
@@ -19,9 +19,11 @@ def build_index(
 ) -> tuple[Path, FaissVectorStore]:
     logger = logging.getLogger("tasks")
     files = get_files_in_folder(Path(kwargs["input_docs_dir"]))
+    for file in files:
+        logger.debug("Found file: %s", file)
     if len(files) == 0:
         raise ValueError(f"No files found in the input folder {kwargs['input_docs_dir']}")
-    return build_index_from_files(files, Path(kwargs["output_index_dir"]))
+    return build_index_from_files(files, Path(kwargs["output_dir"]))
 
 def build_index_from_files(
     files: list[Path],
